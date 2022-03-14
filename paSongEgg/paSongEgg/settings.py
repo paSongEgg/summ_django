8aeec2d602b81b3de88c5b4378900f8003f91271
Monu <kdie44@gmail.com>
2022-03-14 22:33:06 +0900

    중간 백업, postgreSQL-Django 연결 작업 중

--- paSongEgg/paSongEgg/settings.py
+++ paSongEgg/paSongEgg/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'summ',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'paSongEgg.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'summ',
+        'USER':'paSongE',
+        'PASSWORD':'summ2022',
+        'HOST':'localhost',
+        'PORT':'',
     }
 }
 
@@ -121,3 +127,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ORIGIN_ALLOW_ALL=True
--- paSongEgg/summ/models.py
+++ paSongEgg/summ/models.py
@@ -1,3 +1,13 @@
+from django.contrib.postgres.fields import ArrayField  
 from django.db import models
 
 # Create your models here.
+# 임시 작성
+class NewsTable(models.Model):
+    date=models.DateField()
+    author=models.TextField()
+    title=models.TextField()
+    link=models.TextField()
+    naver_link=models.TextField()
+    content=models.TextField()
+    keyword=ArrayField(models.CharField(max_length=100))
